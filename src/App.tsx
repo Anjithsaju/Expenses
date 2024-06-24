2f07f13335f4bfbb13a3ed1e542c66c9aab7b217
Anjithsaju <anjithsaju123@gmail.com>
2024-06-24 16:15:32 +0530

    iiui

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,17 @@ function App() {
         <div>
           <ExpenseSheet />
         </div>
+        <div style={{ display:'flex',alignItems: 'center', margin: '10px',justifyContent: "center" }}>
+        <a className="btn btn-primary" href="https://anjithsaju.github.io/" role="button" style={{ background:"green",border:"none", margin: '20px' }}>Home</a>
+        <a className="btn btn-primary" href="reset.html" role="button" style={{border:"none", margin: '20px' }}>Reset</a>
+        </div>
       </div>
     </>
   );
 }
 export default App;
+/* <a href="https://anjithsaju.github.io/" target="_blank">Home page</a>
+<a href="reset.html">Click me to reset</a> */
 // <div>
 //   <ListGroup
 //     items={items}
